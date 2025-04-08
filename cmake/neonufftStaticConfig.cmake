7c5e52c6f4af50ddf7e2ef59ed0cf4a001445d0f
Simon Frasch <simon.frasch@cscs.ch>
2025-04-08 14:56:28 +0200

    add HIP build option - untested

--- cmake/neonufftStaticConfig.cmake
+++ cmake/neonufftStaticConfig.cmake
@@ -1,12 +1,22 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(FFTW MODULE REQUIRED)
-find_dependency(FFTWF MODULE REQUIRED)
-find_dependency(hwy CONFIG REQUIRED)
+find_dependency(FFTW MODULE)
+find_dependency(FFTWF MODULE)
+find_dependency(hwy CONFIG)
 if(NEONUFFT_OMP)
-  find_dependency(OpenMP MODULE REQUIRED)
+  find_dependency(OpenMP MODULE)
 elseif(NEONUFFT_TBB)
-  find_dependency(TBB CONFIG REQUIRED)
+  find_dependency(TBB CONFIG)
+endif()
+
+if(NEONUFFT_CUDA)
+	find_dependency(CUDAToolkit MODULE)
+endif()
+
+if(NEONUFFT_ROCM)
+  find_dependency(hip CONFIG)
+	find_dependency(hipfft CONFIG)
+  find_dependency(hipcub CONFIG)
 endif()
 
 # find_dependency may set neonufft_FOUND to false, so only add neonufft if everything required was found
